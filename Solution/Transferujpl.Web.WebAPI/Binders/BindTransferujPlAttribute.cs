72919fca5fd2344ee5fd7af1beb64504cd76b968
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-09-27 12:23:28 +0200

    Added logging

---
+++ Solution/Transferujpl.Web.WebAPI/Binders/BindTransferujPlAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Transferujpl.Web.WebAPI.Binders
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public sealed class BindTransferujPlAttribute : ParameterBindingAttribute
+    {
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentException("Invalid parameter");
+
+            return new TransferujPlResponseModelBinder(parameter);
+        }
+    }
+}
--- Solution/Transferujpl.Web.WebAPI/Binders/TransferujPlResponseModelBinder.cs
+++ Solution/Transferujpl.Web.WebAPI/Binders/TransferujPlResponseModelBinder.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Common.Logging;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Metadata;
 using Transferujpl.Core;
 
-namespace Transferujpl.Web.Mvc.Binders
+namespace Transferujpl.Web.WebAPI.Binders
 {
     public class TransferujPlResponseModelBinder : HttpParameterBinding
     {
@@ -14,16 +15,21 @@ namespace Transferujpl.Web.Mvc.Binders
 
         public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken)
         {
+            ILog log = LogManager.GetLogger(typeof(TransferujPlResponseModelBinder));
+            log.Info("ExecuteBindingAsync");
             var binding = actionContext.ActionDescriptor.ActionBinding;
 
             if (actionContext.Request.Headers.Host != "195.149.229.109")
             {
+                log.InfoFormat("Invalid IP - Expecting 195.149.229.109, got: {0}", 
+                                        actionContext.Request.Headers.Host);
                 throw new InvalidOperationException("Invalid IP.");
             }
 
             var type = binding.ParameterBindings[0].Descriptor.ParameterType;
             if (type != typeof(TransferujPlResponse))
             {
+                log.Info("Invalid parameter type. Expecting TransferujPlResponse");
                 throw new InvalidOperationException();
             }
 
@@ -32,6 +38,7 @@ namespace Transferujpl.Web.Mvc.Binders
                     .ContinueWith((task) =>
                     {
                         var stringResult = task.Result;
+                        log.DebugFormat("Detailed info: {0}", stringResult);
                         SetValue(actionContext, new TransferujPlResponse());
                     });
         }
